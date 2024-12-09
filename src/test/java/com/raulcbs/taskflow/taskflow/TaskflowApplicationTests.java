2117ad1c6a5dbb57e995c1025471ff1cd68c7158
raulcbs <raulcubas2016@gmail.com>
2024-12-09 20:44:03 +0000

    Initial commit

---
+++ src/test/java/com/raulcbs/taskflow/taskflow/TaskflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raulcbs.taskflow.taskflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
